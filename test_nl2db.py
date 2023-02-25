86563900851505f140d86f2de538fdeba6cfecd1
Bimi1804 <m.bimassl@gmx.at>
2023-02-25 22:28:11 +0100

    filter for gen and comp

--- test_nl2db.py
+++ test_nl2db.py
@@ -19,7 +19,78 @@ with open(os.getcwd()+"\\test_cases\\test_requirements_NL.txt") as file:
 		l = l.replace("\n","")
 		lines_clean.append(l)
 
-print(lines_clean[0])
+#------------------------------------
+import spacy
+import pandas as pd
+# Load the "en_core_web_sm" pipeline
+nlp = spacy.load("en_core_web_sm")
+
+test_line = nlp(lines_clean[10])
+
+# Remove determiniter and punctuation:
+lines_clean2 = []
+for i in lines_clean:
+	doc = nlp(i)
+	line = []
+	pos_line=[]
+	for token in doc:
+		if token.pos_ != "DET" and token.pos_ != "PUNCT":
+			line.append(token)
+		pos_line.append(token.pos_)
+	#print(line)
+	#print(pos_line)
+	#print("")
+	lines_clean2.append(line)
+
+# Filter attributes:
+lines_clean3 = []
+lines_attr = []
+for line in lines_clean2:
+	for token in line:
+		if token.lemma_ == "have":
+			lines_attr.append(line)
+			break
+	else:
+		lines_clean3.append(line)
+
+
+# Filter generalization and composition:
+lines_clean4 = []
+lines_gen_and_comp = []
+
+for line in lines_clean3:
+	for token in line:
+		if token.dep_ == "ROOT":
+			if token.lemma_ == "be":
+				lines_gen_and_comp.append(line)
+				break
+	else:
+		lines_clean4.append(line)
+
+lines_gen = []
+lines_comp = []
+for line in lines_gen_and_comp:
+	line_tok = []
+	line_dep = []
+	line_head_pos = []
+	for token in line:
+		line_tok.append(token)
+		line_dep.append(token.dep_)
+		line_head_pos.append(token.head.text)
+	print(line_tok)
+	print(line_dep)
+	print(line_head_pos)
+	print("")
+
+
+
+
+# Filter active association:
+
+# Filter passive association:
+
+# Filter composition:
+
 
 
 
