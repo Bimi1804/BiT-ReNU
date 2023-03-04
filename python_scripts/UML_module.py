603ab5993975b87d647bb3b336149a13c11808a3
Bimi1804 <m.bimassl@gmx.at>
2023-03-04 21:45:57 +0100

    functionalities in place!!!!

--- python_scripts/UML_module.py
+++ python_scripts/UML_module.py
@@ -56,3 +56,111 @@ class SQL_UML_Transformer():
 
 
 # DB -> UML
+class UML_SQL_Transformer():
+	def __init__(self):
+		pass 
+
+	def plantuml_to_sql(self,plant_txt):
+		plant_txt = plant_txt.replace(" ","")
+		#-------------------------
+		sql_statemens = []
+		while "class" in plant_txt:
+			class_start = plant_txt.find("class")
+			class_content_start = plant_txt.find("{")
+			class_content_end = plant_txt.find("}")
+			class_name = plant_txt[class_start+5:class_content_start]
+			class_content = plant_txt[class_content_start+1:class_content_end].replace("\n","")
+			plant_txt = plant_txt[class_content_end+1:]
+			class_attr = []
+			class_op = []
+			while "+" in class_content:
+				plus_index = []
+				for i in range(len(class_content)):
+					if class_content[i] == "+":
+						plus_index.append(i)
+				if len(plus_index) > 1:
+					start = plus_index[0]
+					end = plus_index[1]
+					item = class_content[start+1:end]
+					class_content = class_content[plus_index[1]:]
+				elif len(plus_index) == 1:
+					item = class_content[plus_index[0]+1:]
+					class_content = ""
+				if "()" in item:
+					class_op.append(item)
+				if "()" not in item:
+					class_attr.append(item)
+			sql_statemens.append(f"""INSERT OR IGNORE INTO classes(class_name) VALUES ('{class_name.replace(" ","")}')""")
+			for attr in class_attr:
+				if attr != "":
+					sql_statemens.append(f"""INSERT OR IGNORE INTO attributes(attr_name,class_name) VALUES ('{attr.replace(" ","")}','{class_name.replace(" ","")}')""")
+			op_name_class_b = []
+			for op in class_op:
+				upper = []
+				for i in range(len(op)):
+					if op[i].isupper() is True:
+						upper.append(i)
+				op_name = op[:upper[0]]
+				op_class_b = op[upper[0]:].replace("()","")
+				sql_statemens.append(f"""INSERT OR IGNORE INTO classes(class_name) VALUES ('{op_class_b.replace(" ","")}')""")
+				sql_statemens.append(f"""INSERT OR IGNORE INTO operations(op_name,class_name,class_b) 
+					VALUES ('{op}','{class_name.replace(" ","")}','{op_class_b.replace(" ","")}')""")
+
+
+		#-----------------------------------------------------
+		generalization = []
+		composition = []
+		plant_asc_lines = plant_txt.splitlines()
+		for line in plant_asc_lines:
+			if "<|--" in line:
+				generalization.append(line)
+				plant_txt = plant_txt.replace(line,"")
+			if "*--" in line:
+				composition.append(line)
+				plant_txt = plant_txt.replace(line,"")
+
+		for gen in generalization:
+			arrow = gen.find("<|--")
+			sql_statemens.append(f"""INSERT OR IGNORE INTO associations(asc_name,asc_type,class_a,class_b) 
+											VALUES ('inherits','generalization', '{gen[arrow+4:]}','{gen[:arrow]}')""")
+
+		for comp in composition:
+			arrow = comp.find("*--")
+			sql_statemens.append(f"""INSERT OR IGNORE INTO associations(asc_name,asc_type,
+									mult_a_1,mult_a_2,mult_b_1,mult_b_2,class_a,class_b) 
+									VALUES ('is part of','composition','0','*','1','1',
+									'{comp[arrow+3:]}','{comp[:arrow]}')""")
+
+
+
+
+		plant_asc_lines = plant_txt.splitlines()
+		associations = []
+		for line in plant_asc_lines:
+			if line != "" and line != "@enduml":
+				associations.append(line)
+		for line in associations:
+			pointer = line.find('"')
+			class_a = line[:pointer]
+			line = line[pointer:]
+			pointer = line.find("..")
+			mult_a_1 = line[:pointer].replace('"',"")
+			line = line[pointer+2:]
+			pointer = line.find('"')
+			mult_a_2 = line[:pointer].replace('"',"")
+			line = line[pointer+1:]
+			line = line[line.find('"')+1:]
+			pointer = line.find("..")
+			mult_b_1 = line[:pointer]
+			line = line[pointer:]
+			pointer = line.find('"')
+			mult_b_2 = line[:pointer].replace("..","")
+			line = line[pointer+1:]
+			pointer = line.find(":")
+			class_b = line[:pointer]
+			asc_name = line[pointer+1:]
+			sql_statemens.append(f"""INSERT OR IGNORE INTO associations
+									(asc_type,asc_name,class_a,class_b,mult_a_1,mult_a_2,mult_b_1,mult_b_2) 
+									VALUES ('association','{asc_name}','{class_a}','{class_b}','{mult_a_1}','{mult_a_2}','{mult_b_1}','{mult_b_2}')""")
+		return sql_statemens
+
--- test_db2uml.py
+++ test_db2uml.py
@@ -13,7 +13,7 @@ NL_filter = NL_Filter()
 NL_SQL_transformer = NL_SQL_Transformer()
 SQL_NL_morph = SQL_NL_Transformer()
 SQL_UML_transformer = SQL_UML_Transformer()
-
+UML_SQL_transformer = UML_SQL_Transformer()
 
 db_mod.delete_db_file("test_req1")
 
@@ -22,150 +22,33 @@ db_mod.set_curr_project("test_req1")
 
 
 with open(os.getcwd()+"\\test_cases\\plant_test_requirements.txt") as file:
-	plantuml = file.read()
+	plant_text = file.read()
+
+sql_statemens = UML_SQL_transformer.plantuml_to_sql(plant_text)
+
+db_mod.write_to_db(sql_statemens)
+
+dataframes = db_mod.read_all_db()
+plant_dia = SQL_UML_transformer.sql_to_plantuml(dataframes)
+print(plant_dia)
 
 
 
-#print(db_mod.read_all_db())
+counter = 1
+for i in SQL_NL_morph.attr_to_nl(dataframes[1]):
+	print(f"{counter}: {i}")
+	counter = counter +1
 
-#print(plantuml)
+gen_sent, comp_sent, asc_sent = SQL_NL_morph.asc_to_nl(dataframes[3])
+for i in gen_sent:
+	print(f"{counter}: {i}")
+	counter = counter +1
+for i in comp_sent:
+	print(f"{counter}: {i}")
+	counter = counter +1
+for i in asc_sent:
+	print(f"{counter}: {i[0]}")
+	counter = counter +1
+	print(f"{counter}: {i[1]}")
+	counter = counter +1
 
-classes = []
-attributes = []
-operations = []
-associations = []
-
-while "class" in plantuml:
-	class_start = plantuml.find("class")
-	class_content_start = plantuml.find("{")
-	class_content_end = plantuml.find("}")
-	class_name = plantuml[class_start+6:class_content_start]
-	class_content = plantuml[class_content_start+1:class_content_end]
-	plantuml = plantuml[class_content_end+1:]
-	class_attr = []
-	class_op = []
-	while "+" in class_content:
-		attr_op_start = class_content.find("+")
-		if "+" in class_content[attr_op_start+2:]:
-			attr_op_end = class_content[attr_op_start+1:].find("+")
-		else:
-			attr_op_end = len(class_content)
-		item = class_content[attr_op_start+2:attr_op_end+1]
-		item = item.replace("\n","")
-		if "()" in item:
-			class_op.append(item)
-		if "()" not in item:
-			class_attr.append(item)
-		if "+" in class_content[attr_op_start+2:]:
-			class_content = class_content[attr_op_end+2:]
-		else:
-			class_content = ""
-	classes.append(class_name)
-	attributes.append([class_name,class_attr])
-	op_name_class_b = []
-	for op in class_op:
-		upper = []
-		for i in range(len(op)):
-			if op[i].isupper() is True:
-				upper.append(i)
-		op_name = op[:upper[0]]
-		op_class_b = op[upper[0]:].replace("()","")
-		op_name_class_b.append([op_name,op_class_b])
-
-
-	operations.append([class_name,op_name_class_b])
-#--------------------------------------------------
-print("CLASSES:")
-for i in classes:
-	print(i)
-
-print("\nATTRIBUTES:")
-for i in attributes:
-	if i[1] != []:
-		print(f"{i[0]}: {i[1]}")
-
-print("\nOPERATIONS:")
-for i in operations:
-	if i[1] != []:
-		print(f"{i[0]}: {i[1]}")
-print("")
-#-----------------------------------------------------
-
-generalization = []
-composition = []
-plant_asc_lines = plantuml.splitlines()
-for line in plant_asc_lines:
-	if "<|--" in line:
-		generalization.append(line)
-		plantuml = plantuml.replace(line,"")
-	if "*--" in line:
-		composition.append(line)
-		plantuml = plantuml.replace(line,"")
-
-print("generalization:")
-gen = []
-for i in generalization:
-	arrow = i.find("<|--")
-	gen.append([i[:arrow-1],i[arrow+5:]])
-for i in gen:
-	print(i)
-
-
-print("\ncomposition:")
-comp = []
-for i in composition:
-	arrow = i.find("*--")
-	comp.append([i[:arrow-1],i[arrow+4:]])
-for i in comp:
-	print(i)
-
-
-
-plant_asc_lines = plantuml.splitlines()
-associations = []
-for line in plant_asc_lines:
-	if line != "" and line != "@enduml":
-		associations.append(line)
-
-
-print("\nAssociations:")
-for i in associations:
-	line = i.replace(" ","")
-	#asc_name = ""
-	#class_a = ""
-	#class_b = ""
-	#mult_a_1 = ""
-	#mult_a_2 = ""
-	#mult_b_1 = ""
-	#mult_b_2 = ""
-	pointer = line.find('"')
-	class_a = line[:pointer]
-	line = line[pointer:]
-	pointer = line.find("..")
-	mult_a_1 = line[:pointer].replace('"',"")
-	line = line[pointer+2:]
-	pointer = line.find('"')
-	mult_a_2 = line[:pointer].replace('"',"")
-	line = line[pointer+1:]
-	line = line[line.find('"')+1:]
-	pointer = line.find("..")
-	mult_b_1 = line[:pointer]
-	line = line[pointer:]
-	pointer = line.find('"')
-	mult_b_2 = line[:pointer].replace("..","")
-	line = line[pointer+1:]
-	pointer = line.find(":")
-	class_b = line[:pointer]
-	asc_name = line[pointer+1:]
-
-
-
-#print(plantuml[class_content_start+1:class_content_end])
-
-
-
-
-
-plantuml = plantuml[class_content_end+1:]
-#print("-----------------")
-#print(plantuml)
