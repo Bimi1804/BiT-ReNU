f5b2be1e4a98f0f93fd1ba2babcfa50e0bf56b91
Bimi1804 <m.bimassl@gmx.at>
2023-03-04 18:19:07 +0100

    Zwischenspeichern

--- test_db2uml.py
+++ test_db2uml.py
@@ -15,11 +15,71 @@ SQL_NL_morph = SQL_NL_Transformer()
 SQL_UML_transformer = SQL_UML_Transformer()
 
 
+db_mod.delete_db_file("test_req1")
+
+db_mod.create_new_project("test_req1")
 db_mod.set_curr_project("test_req1")
 
 
-dataframes = db_mod.read_all_db()
-print(SQL_UML_transformer.sql_to_plantuml(dataframes))
+with open(os.getcwd()+"\\test_cases\\plant_test_requirements.txt") as file:
+	plantuml = file.read()
 
 
 
+#print(db_mod.read_all_db())
+
+#print(plantuml)
+
+classes = []
+attributes = []
+operations = []
+associations = []
+
+while "class" in plantuml:
+	class_start = plantuml.find("class")
+	class_content_start = plantuml.find("{")
+	class_content_end = plantuml.find("}")
+	class_name ={plantuml[class_start+6:class_content_start]}
+	class_content = plantuml[class_content_start+1:class_content_end]
+	plantuml = plantuml[class_content_end+1:]
+	 
+	class_attr = []
+	class_op = []
+	while "+" in class_content:
+		attr_op_start = class_content.find("+")
+		if "+" in class_content[attr_op_start+2:]:
+			attr_op_end = class_content[attr_op_start+1:].find("+")
+		else:
+			attr_op_end = len(class_content)
+		item = class_content[attr_op_start+2:attr_op_end+1]
+		item = item.replace("\n","")
+		if "()" in item:
+			class_op.append(item)
+		if "()" not in item:
+			class_attr.append(item)
+		if "+" in class_content[attr_op_start+2:]:
+			class_content = class_content[attr_op_end+2:]
+		else:
+			class_content = ""
+	classes.append(class_name)
+	attributes.append([class_name,class_attr])
+
+
+print(classes)
+
+print("-------------------")
+print(attributes)
+
+
+
+
+
+#print(plantuml[class_content_start+1:class_content_end])
+
+
+
+
+
+plantuml = plantuml[class_content_end+1:]
+#print("-----------------")
+#print(plantuml)
